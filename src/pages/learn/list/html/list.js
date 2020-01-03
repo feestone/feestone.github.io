01b783b50a22a4cf6253375bced216f45d85f767
feestOne <13241317776@163.com>
2020-01-03 18:41:12 +0800

    增加HTML02

--- src/pages/learn/list/html/list.js
+++ src/pages/learn/list/html/list.js
@@ -1,5 +1,5 @@
 var pageList = [{
-    "title": "初始HTML，HTML的常用标签，路径。",
+    "title": "HTML的常用标签，路径。",
     "time": "2020-01-02 11:32",
     "Introduction": "主要介绍页面的组成，常用浏览器，HTML的常用标签，路径的相关知识。",
     "todetailUrl": "/htmlList/html01"
