7944a1eba9ceee2ed43d1867b59de1f677d87b5d
feestOne <13241317776@163.com>
2020-01-15 17:49:56 +0800

    1

--- src/pages/life/list/list.js
+++
@@ -1,9 +0,0 @@
-var pageList = [{
-    "title": "25岁的我。",
-    "time": "2020-01-15 17:11",
-    "Introduction": "现在的处境和想法。",
-    "todetailUrl": "/life/life01"
-}, ]
-export default {
-    pageList: pageList
-}
